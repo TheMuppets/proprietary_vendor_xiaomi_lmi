bb7ce39d37d4272b8fda418940c8430203ebfd25
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-04-02 01:54:41 +0200

    lmi: Drop 64-bit soundtrigger blobs
    Change-Id: I85590f7ae7629e2088df951b55651b0fa9c1f90c

--- lmi-vendor.mk
+++ lmi-vendor.mk
@@ -333,9 +333,6 @@ PRODUCT_PACKAGES += \
     libcamxstatscore \
     libcamxswprocessalgo \
     libcamxtintlessalgo \
-    libcapiv2svacnn \
-    libcapiv2svarnn \
-    libcapiv2vop \
     libchilog \
     libcom.qti.chinodeutils \
     libcvp2 \
